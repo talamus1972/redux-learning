3cb5d43b6996ffac6b581e250b35572337f58e81
talamus1972 <talamus1972@gmail.com>
2024-03-15 20:16:25 +0200

    11

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,19 @@
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector((state) => state.filters.status);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
